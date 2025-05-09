477465db8b51641d059fd994d73f1ab0576326bf
mathiasValenzuela <132649598+mathiasValenzuela@users.noreply.github.com>
2025-05-08 23:00:48 -0400

    cambios y actualizacion de aplicacion a la version 5
    se incluyen mejoras visuales y chatbot con inteligencia artificial a la pagina de dashboard.

--- DOCUMENTOS FASE2/test/static/scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Funcionalidad adicional para el dashboard
-    console.log("Dashboard cargado");
-    
-    // Puedes añadir más interacciones aquí
-});
